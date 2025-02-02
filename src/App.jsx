dc6806d8c89b06691502e8a1d376c657c073d041
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-02 14:27:45 +0530

    applied loading animation by using useNavigation hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
