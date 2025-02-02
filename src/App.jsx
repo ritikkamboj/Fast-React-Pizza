3dbaa12a3a853b96566eebac778ccabe8403f05a
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-02 17:31:38 +0530

    Fetching the orders details by using loader function, useNavigation,useLoaderData,

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import "./index.css";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -21,6 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -31,8 +34,10 @@ const router = createBrowserRouter([
         element: <CreateOrder />,
       },
       {
-        path: "/order/orderId",
+        path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import {
+  calcMinutesLeft,
+  formatCurrency,
+  formatDate,
+} from "../../utilities/helpers";
+import { getOrder } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
 
 // Test ID: IIDSAT
 
@@ -8,42 +15,10 @@ import React from "react";
 //   formatDate,
 // } from "../../utils/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
 function Order() {
+  const order = useLoaderData();
+  console.log(order);
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -83,6 +58,11 @@ function Order() {
       </div>
     </div>
   );
+}
+
+export async function loader({ params }) {
+  const order = await getOrder(params.orderId);
+  return order;
 }
 
 export default Order;
