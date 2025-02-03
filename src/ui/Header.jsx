cb8955928222726ef5a053a1b08c16e77ce40e17
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-03 20:15:04 +0530

    added responsiveness in code

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="p border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="p border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
