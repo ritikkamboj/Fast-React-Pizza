9584e61bc74c74b90ff2c7dfa66e1dbe1cae360a
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-04 13:16:59 +0530

    applied grid in home.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import React from 'react';
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   console.log(navigation);
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <h1>Content </h1>
-        <p>------------------------------------</p>
-        <Outlet />
-        <p>---------------------------------------</p>
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl bg-red-500">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
