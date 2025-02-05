75b8d17d1bade8a8bd507850468ad1978e6e7d0b
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-05 19:25:44 +0530

    reusing styles

---
+++ src/ui/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to }) {
+  const classname =
+    'rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4 inline-block';
+  if (to) {
+    return (
+      <Link to={to} className={classname}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button disabled={disabled} className={classname}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,5 +1,6 @@
-import { useNavigate, useRouteError } from "react-router-dom";
-import React from "react";
+import { useNavigate, useRouteError } from 'react-router-dom';
+import React from 'react';
+import LinkButton from './LinkButton';
 function Error() {
   const navigate = useNavigate();
   const err = useRouteError();
@@ -10,7 +11,7 @@ function Error() {
       <h1>Something went wrong 😢</h1>
       <p>{err.data}</p>
       <p>{err.message}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
 }
