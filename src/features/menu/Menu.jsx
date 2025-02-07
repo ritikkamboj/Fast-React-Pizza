7c7147de88bff48e81b8e701d68ff5a618c144f7
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-07 17:31:55 +0530

    applied styling to menu page and make the use of button component more versatile

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { getMenu } from "../../services/apiRestaurant";
-import { useLoaderData } from "react-router-dom";
-import MenuItem from "./MenuItem";
+import React from 'react';
+import { getMenu } from '../../services/apiRestaurant';
+import { useLoaderData } from 'react-router-dom';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
   return (
-    <ul>
+    <ul className="divide-y divide-stone-300 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const classname =
-    'rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4 inline-block';
+function Button({ children, disabled, to, type }) {
+  // const classname =
+  //   'rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4 inline-block';
+
+  const base =
+    'rounded-full bg-yellow-400  font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed  inline-block';
+
+  const styles = {
+    primary: base + ' px-4 px-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+  };
+
   if (to) {
     return (
-      <Link to={to} className={classname}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
   }
   return (
-    <button disabled={disabled} className={classname}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
