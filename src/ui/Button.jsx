ba73f02cfecaaa3416246560ed9bd119b5859aba
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-07 20:54:38 +0530

    style the order and cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,11 +6,13 @@ function Button({ children, disabled, to, type }) {
   //   'rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4 inline-block';
 
   const base =
-    'rounded-full bg-yellow-400  font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed  inline-block';
+    ' text-sm rounded-full bg-yellow-400  font-semibold uppercase tracking-wider text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed  inline-block';
 
   const styles = {
-    primary: base + ' px-4 px-3 md:px-6 md:py-4',
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'rounded-full text-sm  font-semibold uppercase tracking-wider text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed  inline-block border-2 border-stone-300 px-4 py-2.5 md:px-6 md:py-3.5 hover:text-stone-800 focus:text-stone-800',
   };
 
   if (to) {
