9fda3cbcbfffacab3919a7afc45cd9946fb6c87c
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-02-09 11:16:32 +0530

    Reading and updating the userState

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -5,6 +5,7 @@ import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import { list } from 'postcss';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const fakeCart = [
   {
@@ -31,13 +32,15 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector((state) => state.user.username);
+
   const cart = fakeCart;
 
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.pizzaId} />
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -36,6 +37,7 @@ const fakeCart = [
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const username = useSelector((state) => state.user.username);
 
   const navigation = useNavigation();
   console.log(navigation);
@@ -55,7 +57,13 @@ function CreateOrder() {
           <label className="sm:basis-40">First Name</label>
           {/* <input type="text" name="customer" required className="input" /> */}
           <div className="grow">
-            <input className="input" type="text" name="address" required />
+            <input
+              className="input"
+              type="text"
+              name="address"
+              required
+              defaultValue={username}
+            />
           </div>
         </div>
 
